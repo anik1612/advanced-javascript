eaea79133840d9f3c412c11539c64d8ec8b6405a
Anik Sarker <aniksarker1612@gmail.com>
2020-08-07 17:46:40 +0800

    comment remove from map-filter.js

--- map-filter.js
+++ map-filter.js
@@ -1,69 +1,69 @@
-// const numbers = [3, 4, 5, 6, 7, 8];
-// const output = [];
+const numbers = [3, 4, 5, 6, 7, 8];
+const output = [];
 
-// for(let i=0; i<numbers.length; i++){
-//     const element = numbers[i];
-//     const result = element * element;
-//     output.push(result);
-// }
-
-
-//map 
-// function square(element) {
-//     return element * element;
-// }
-
-// const square = element => element * element;
-// const square = x => x * x;
-
-// const result = numbers.map(function (element) {
-//     return element * element;
-//     // console.log(element)
-// })
-
-// const result = numbers.map(x => x * x)
-
-// console.log(result);
+for(let i=0; i<numbers.length; i++){
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
 
 
-//filter
-// const bigger = numbers.filter(x => x > 5);
+map 
+function square(element) {
+    return element * element;
+}
 
-// const find = numbers.find(x => x > 5)
+const square = element => element * element;
+const square = x => x * x;
+
+const result = numbers.map(function (element) {
+    return element * element;
+    // console.log(element)
+})
+
+const result = numbers.map(x => x * x)
+
+console.log(result);
+
+
+filter
+const bigger = numbers.filter(x => x > 5);
+
+const find = numbers.find(x => x > 5)
  
-// console.log(find);
+console.log(find);
 
-//foreach
-// const days = ['mon', 'sun', 'tue', 'wed', 'thr', 'fri', 'sat']
+foreach
+const days = ['mon', 'sun', 'tue', 'wed', 'thr', 'fri', 'sat']
 
-// // console.log(days[0]);
+// console.log(days[0]);
 
-// days.forEach(function(day, index){
-//     console.log(day, index)
-// })
+days.forEach(function(day, index){
+    console.log(day, index)
+})
 
-// const arr = [10, 20, 30, 40, 50];
+const arr = [10, 20, 30, 40, 50];
 
-// arr.forEach(function(num, index){
-//     console.log(num, index);
-// })
+arr.forEach(function(num, index){
+    console.log(num, index);
+})
 
-// arrow function
-// const result = (x) => x * x;
+arrow function
+const result = (x) => x * x;
 
-// console.log(result(5));
+console.log(result(5));
 
-// square a number using foreach
+square a number using foreach
 let arr = [10, 20, 30, 40, 50];
 const arrCpy = [];
 
-// // arr.forEach(function(x){
-// //     arrCpy.push(x*x);
-// // })
+// arr.forEach(function(x){
+//     arrCpy.push(x*x);
+// })
 
-// arr.forEach(x => arrCpy.push(x*x));
+arr.forEach(x => arrCpy.push(x*x));
 
-// console.log(arrCpy)
+console.log(arrCpy)
 
 arr.forEach(x => arrCpy.push(x*x));
 console.log(arrCpy);
